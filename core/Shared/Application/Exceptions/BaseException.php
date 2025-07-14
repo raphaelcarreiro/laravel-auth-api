f17931662b15375c800aabcf8467ff2e8bcd7adb
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-07-14 14:10:45 -0300

    refactor: enhance logging in BaseException class

--- core/Shared/Application/Exceptions/BaseException.php
+++ core/Shared/Application/Exceptions/BaseException.php
@@ -2,17 +2,29 @@
 
 namespace Core\Shared\Application\Exceptions;
 
+use Core\Shared\Infra\Logger\LoggerTrait;
 use Exception;
 use Illuminate\Http\JsonResponse;
 
 abstract class BaseException extends Exception
 {
+    use LoggerTrait;
+
     abstract function toArray(): array;
 
     abstract function getStatusCode(): int;
 
     public function report(): bool
     {
+        $this->logger()->error($this->getMessage(), [
+            'extra' => [
+                'code' => $this->getCode(),
+                'file' => $this->getFile(),
+                'line' => $this->getLine(),
+                'trace' => $this->getTraceAsString(),
+            ]
+        ]);
+
         return true;
     }
 
