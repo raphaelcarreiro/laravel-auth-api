143ee0d615d408c130d8540d18bb2d4c661a398d
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-07-15 01:46:28 -0300

    feat: register OpentelemetryProvider in AppServiceProvider

--- app/Modules/Shared/Providers/AppServiceProvider.php
+++ app/Modules/Shared/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Shared\Providers;
 
+use App\Modules\Opentelemetry\Providers\OpentelemetryProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,6 +10,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->register(ModuleServiceProvider::class);
+        $this->app->register(OpentelemetryProvider::class);
     }
 
     public function boot(): void
