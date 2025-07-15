a345d5198980614b8a691958ba1be1161deac205
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-07-15 01:46:39 -0300

    feat: add OpentelemetryMiddleware and update command registration in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Modules\Audit\Middlewares\AuditMiddleware;
+use App\Modules\Opentelemetry\Middlewares\OpentelemetryMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use App\Modules\Audit\Commands\AuditConsumerCommand;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,9 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(AuditMiddleware::class);
+        $middleware->append(OpentelemetryMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request) {
             return true;
         });
-    })->create();
+    })
+    ->withCommands([
+        AuditConsumerCommand::class,
+    ])
+    ->create();
