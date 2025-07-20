f3b96518a1412e4ce8ac30782b3513c693489818
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-07-20 15:28:32 +0000

    feat: enhance exception handling to log errors and return JSON response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,13 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use App\Modules\Audit\Commands\AuditConsumerCommand;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -21,6 +23,27 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request) {
             return true;
+        });
+
+        $exceptions->renderable(function (Throwable $e, Request $request) {
+            if (!app()->environment('production')) {
+                return null;
+            }
+
+            Log::error($e->getMessage(), [
+                'extra' => [
+                    'exception' => get_class($e),
+                    'message' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                    'trace' => collect($e->getTrace())->take(5),
+                ]
+            ]);
+
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'internal server error'
+            ], 500);
         });
     })
     ->withCommands([
