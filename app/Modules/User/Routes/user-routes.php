49df378e9e2302b20185b5dedd20eb23cb5ab0a1
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-08-11 12:47:12 -0300

    feat: add route for WhoamiController to retrieve current user information

--- app/Modules/User/Routes/user-routes.php
+++ app/Modules/User/Routes/user-routes.php
@@ -2,8 +2,10 @@
 
 use App\Modules\Auth\Middlewares\AuthMiddleware;
 use App\Modules\User\Controllers\CreateUserController;
+use App\Modules\User\Controllers\WhoamiController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('users.')->middleware(AuthMiddleware::class)->group(function () {
     Route::post('/users', [CreateUserController::class, 'index'])->name('create');
+    Route::get('/whoami', [WhoamiController::class, 'index'])->name('whoami');
 });
